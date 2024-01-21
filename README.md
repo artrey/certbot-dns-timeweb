bfa9b49c0859438636ac5759c77476bd19fc08df
Alexander Ivanov <oz.sasha.ivanov@gmail.com>
2024-01-21 21:31:56 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# certbot-dns-timeweb
+
+Timeweb DNS Authenticator plugin for Certbot
+
+## Installation
+
+```bash
+pip install certbot-dns-timeweb
+```
+
+## Getting started
+
+Get Timeweb Cloud [access token](https://timeweb.cloud/my/api-keys) and fill credentials configuration.
+
+Then issue a certificate with command like:
+
+```bash
+certbot certonly --authenticator dns-timeweb \
+    --dns-timeweb-credentials /etc/letsencrypt/timeweb-creds.ini \
+    -d example.org -d *.example.org
+```
+
+## Plugin arguments
+
+- `--dns-timeweb-credentials` - path to Credentials configuration.
+- `--dns-timeweb-propagation-seconds` - seconds when DNS record is propagated (default: 10)
+
+## Configuration example
+
+```ini
+# /etc/letsencrypt/timeweb-creds.ini is a suggested path for
+# configuration file. You may place him in any place.
+dns_timeweb_api_key = XXXXXXXXXXXXXXXXXXX
+```
---
+++ certbot_dns_timeweb/authenticator.py
@@ -0,0 +1,64 @@
+"""
+Timeweb DNS Authenticator
+"""
+import collections
+import functools
+import logging
+from typing import Callable
+
+from certbot.plugins import dns_common
+from timeweb import Timeweb
+
+from certbot import errors
+
+logger = logging.getLogger(__name__)
+
+
+class Authenticator(dns_common.DNSAuthenticator):
+    """
+    Timeweb DNS Authenticator
+
+    This Authenticator uses the Timeweb Cloud API to fulfill a DNS-01 challenge.
+    """
+
+    description = "Obtain certificates using a DNS TXT record using Timeweb DNS API."
+    ttl = 300
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.credentials = None
+        self._for_cleanup = collections.defaultdict(list)
+
+    @classmethod
+    def add_parser_arguments(cls, add: Callable[..., None], *args, **kwargs) -> None:
+        super().add_parser_arguments(add, *args, **kwargs)
+        add("credentials", help="Timeweb credentials INI file.")
+
+    def more_info(self) -> str:
+        return "This plugin configures a DNS TXT record to respond to a DNS-01 challenge using the Timeweb DNS API."
+
+    def _setup_credentials(self) -> None:
+        self.credentials = self._configure_credentials(
+            "credentials",
+            "Timeweb credentials INI file",
+            {"api_key": "Timeweb API key"},
+        )
+
+    def _perform(self, domain: str, validation_name: str, validation: str) -> None:
+        response = self._timeweb_client.domains.add_dns_record(
+            fqdn=domain,
+            type="TXT",
+            value=validation,
+            subdomain=validation_name.rstrip(domain).rstrip("."),
+        )
+        self._for_cleanup[validation_name].append(response.dns_record.id)
+
+    def _cleanup(self, domain: str, validation_name: str, validation: str) -> None:
+        if validation_name not in self._for_cleanup:
+            return
+        for record_id in self._for_cleanup.pop(validation_name):
+            self._timeweb_client.domains.delete_dns_record(domain, record_id)
+
+    @functools.cached_property
+    def _timeweb_client(self) -> Timeweb:
+        return Timeweb(token=self.credentials.conf("api_key"))
